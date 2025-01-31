f960900a2a8fc7a631641aa4c38c7f059f0afd15
egarrisxn <egarrisxn@gmail.com>
2025-01-31 11:01:57 -0500

    ogimage

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,43 +12,47 @@ export const metadata = {
   title: 'Unofficial Merchandise',
   description: 'The unofficial ecommerce webstore for Unofficial Merchandise.',
   referrer: 'origin-when-cross-origin',
+  creator: 'https://egxo.dev.',
   keywords: ['sanity', 'next', 'react', 'tailwindcss', 'cms', 'ecommerce', 'egarrisxn'],
   robots: {
     index: true,
-    follow: true,
+    follow: false,
+    nocache: true,
     googleBot: {
       index: true,
-      follow: true,
+      follow: false,
+      noimageindex: true,
       'max-video-preview': -1,
       'max-image-preview': 'large',
       'max-snippet': -1,
     },
   },
+  manifest: '/manifest.json',
   openGraph: {
-    type: 'website',
-    locale: 'en_US',
     title: 'Unofficial Merchandise',
     description: 'The unofficial ecommerce webstore for Unofficial Merchandise.',
     url: 'https://unofficialmerchandise.vercel.app',
     siteName: 'Unofficial Merchandise',
     images: [
       {
-        url: `https://unofficialmerchandise.vercel.app/opengraph-image.png`,
+        url: '/opengraph-image.png',
         width: 1200,
         height: 630,
         alt: 'The unofficial ecommerce webstore for Unofficial Merchandise.',
       },
     ],
+    type: 'website',
+    locale: 'en_US',
   },
   twitter: {
-    cardType: 'summary_large_image',
-    creator: 'eg__xo',
-    site: 'https://twitter.com/eg_xo_',
-    title: 'EG. | Blog',
-    description: 'SPEND A LITTLE TIME INSIDE MY HEAD.',
+    card: 'summary_large_image',
+    title: 'Unofficial Merchandise',
+    description: 'The unofficial ecommerce webstore for Unofficial Merchandise.',
+    creator: '@eg__xo',
+    site: '@eg__xo',
     images: [
       {
-        url: `https://unofficialmerchandise.vercel.app/twitter-image.png`,
+        url: '/twitter-image.png',
         width: 1200,
         height: 630,
         alt: 'The unofficial ecommerce webstore for Unofficial Merchandise.',
@@ -56,7 +60,18 @@ export const metadata = {
     ],
   },
   icons: {
-    shortcut: 'https://unofficialmerchandise.vercel.app/favicon.ico',
+    icon: [
+      {url: '/favicon.ico', sizes: '48x48', type: 'image/x-icon'},
+      {url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png'},
+      {url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png'},
+    ],
+    apple: [{url: '/apple-icon-192x192.png', sizes: '192x192', type: 'image/png'}],
+    other: [
+      {
+        rel: 'apple-icon-precomposed',
+        url: '/apple-icon-precomposed.png',
+      },
+    ],
   },
 }
 
