fb1f4703bbc93a57cb6befd452a0eb3203de0779
egarrisxn <egarrisxn@gmail.com>
2026-05-16 00:14:40 -0400

    more

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path"
-import { fileURLToPath } from "url"
-import { FlatCompat } from "@eslint/eslintrc"
+import {dirname} from 'path'
+import {fileURLToPath} from 'url'
+import {FlatCompat} from '@eslint/eslintrc'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -11,11 +11,28 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: ["next/core-web-vitals", "prettier"],
-    rules: {},
+    extends: ['next/core-web-vitals', 'prettier'],
+    rules: {
+      'no-console': ['warn', {allow: ['warn', 'error']}],
+      'import/no-anonymous-default-export': 'off',
+    },
   }),
   {
-    ignores: ["**/node_modules/", ".git/", ".vscode/", ".next/", "sanity/", "public/"],
+    ignores: [
+      '.next/**',
+      '.git/**',
+      '.vscode/**',
+      '.vercel/**',
+      'coverage/**',
+      'node_modules/**',
+      'dist/**',
+      'sanity/dist/**',
+      'public/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
+      '**/node_modules/',
+    ],
   },
 ]
 
